3a0e410b66298ae1575653ca5f1b56d4022b33d0
Junker <dk-junker@ya.ru>
2016-12-07 01:58:45 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# InfluxDbServiceProvider
+InfluxDb Service Provider for Silex
+
+## Requirements
+silex 1.x
+InfluxDB client library for PHP
+
+##Installation
+The best way to install InfluxDbServiceProvider is to use a [Composer](https://getcomposer.org/download):
+
+    php composer.phar require junker/influxdb-service-provider
+
+## Examples
+
+```php
+use Junker\Silex\Provider\InfluxDbServiceProvider;
+
+$app->register(new InfluxDbServiceProvider(), [
+    'influxdb.host' => 'localhost',
+    'influxdb.port' => 8086,
+    'influxdb.database' => 'stat',
+    'influxdb.username' => 'username',
+    'influxdb.password' => 'password'
+]);
+
+$app['influxdb']->query('select * from test_metric LIMIT 5');
+
+$app['influxdb.client']->selectDB('influx_test_db');
+
+```
+
+## Documentation
+
+[InfluxDB client library for PHP](https://github.com/influxdata/influxdb-php)
---
+++ src/Junker/Silex/Provider/InfluxDbServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Junker\Silex\Provider;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class InfluxDbServiceProvider implements ServiceProviderInterface
+{
+    const PORT = 8086;
+    const HOST = 'localhost';
+
+    public function register(Application $app)
+    {
+        $app['influxdb'] = $app->share(function (Application $app) {
+            return $app['influxdb.client']->selectDB($app['influxdb.database']);
+        });
+
+        $app['influxdb.client'] = $app->share(function (Application $app) {
+            return new InfluxDB\Client(
+                isset($app['influxdb.host']) ? $app['influxdb.host'] : self::HOST,
+                isset($app['influxdb.port']) ? $app['influxdb.port'] : self::PORT,
+                isset($app['influxdb.username']) ? $app['influxdb.username'] : '',
+                isset($app['influxdb.password']) ? $app['influxdb.password'] : ''
+            );
+        });
+    }
+
+    public function boot(Application $app) {
+
+    }
+}
